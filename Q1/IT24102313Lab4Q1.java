623a4940ccb701974d14e74c181d5295eb904ab2
IT24102313 <it24102313@my.sliit.lk>
2024-08-21 21:18:08 +0530

    IT24102313Lab4Q1.java

---
+++ Q1/IT24102313Lab4Q1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class IT24102313Lab4Q1{
+   public static void main(String[]args){
+      Scanner scanner = new Scanner(System.in);
+      System.out.print("Enter a number : ");
+         int num = scanner.nextInt();
+          if (num > 0){
+           System.out.println("The number is: positive");
+              } else if (num < 0){
+                System.out.println("The number is: Negative");
+
+                 }else{ System.out.println("The number is zero");
+
+
+
+}      
+
+}
+
+
+
+}
